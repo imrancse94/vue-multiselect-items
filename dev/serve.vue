e90167c74cd73a7f0a34abc3ae3f61b9c0e376ff
imrancse94 <imran@cloud-height.com>
2021-10-26 21:59:08 +0600

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueMultiselectItems from '@/vue-multiselect-items.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueMultiselectItems
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-multiselect-items />
+  </div>
+</template>
